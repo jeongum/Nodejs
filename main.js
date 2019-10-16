9540f66fcb474494038b561997a92deb6b457ace
leahJU <wjddma1214@gmail.com>
2019-10-16 23:09:30 +0900

    security of input data

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
- 
+var path = require('path');
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -23,7 +24,8 @@ var app = http.createServer(function(request,response){
         });
       } else {
         fs.readdir('./data', function(error, filelist){
-          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var filteredId = path.parse(queryData.id).base;
+          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.id;
             var list = template.list(filelist);
             var html = template.HTML(title, list,
@@ -74,7 +76,8 @@ var app = http.createServer(function(request,response){
       });
     } else if(pathname ==='/update'){
       fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = template.list(filelist);
           var html = template.HTML(title, list,
@@ -121,7 +124,8 @@ var app = http.createServer(function(request,response){
       request.on('end', function(){
           var post = qs.parse(body);
           var id = post.id;
-          fs.unlink(`data/${id}`,function(err){
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`,function(err){
             response.writeHead(302, {Location: `/`});
             response.end();
           });
---
+++ password.js
@@ -0,0 +1,4 @@
+module.exports = {
+    id:'leahJU',
+    password:'1111'
+}
